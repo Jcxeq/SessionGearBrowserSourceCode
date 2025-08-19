ec1e66da1b3f4aa4aadeee0f7d802ec99d1d7849
jc <58092680+Jcxeq@users.noreply.github.com>
2025-08-18 18:25:49 -0700

    Add files via upload

---
+++ SGB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SessionGearInstaller
+{
+    public partial class SGB : Form
+    {
+        public SGB()
+        {
+            InitializeComponent();
+        }
+
+        private void SGI_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
